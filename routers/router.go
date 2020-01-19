d10dd23b2d3989138cfee4d98b30e316079b4c16
mrbuffon <gaoyunqiang00@sina.com>
2020-01-19 16:39:46 +0800

    设置静态页面

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,24 @@
 package routers
 
 import (
-	"MinimalismBlog/controllers"
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	// 重定向静态资源文件
+	beego.SetStaticPath("/img","static/img")
+	beego.SetStaticPath("/css","static/css")
+	beego.SetStaticPath("/js","static/js")
+
+	//重定向静态页面
+	beego.SetStaticPath("index.html", "views/index.html")
+	beego.SetStaticPath("blog_detail.html", "views/blog_detail.html")
+	beego.SetStaticPath("classify.html", "views/classify.html")
+	beego.SetStaticPath("install.html", "views/install.html")
+	beego.SetStaticPath("login.html", "views/login.html")
+	beego.SetStaticPath("modify_pwd.html", "views/modify_pwd.html")
+	beego.SetStaticPath("resume.html", "views/resume.html")
+	beego.SetStaticPath("tag.html", "views/tag.html")
+
+	//接口API路由
 }
